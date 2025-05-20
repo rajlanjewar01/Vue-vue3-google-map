1d04ba29d9507e84985ebca2314f6bd7bd1c1026
rajlanjewar01 <rajl.perficient@gmail.com>
2025-05-20 15:40:22 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="width: 800px">
+    <GoogleMapComponent />
+  </div>
+</template>
+
+<script>
+import GoogleMapComponent from './components/GoogleMapComponent.vue';
+export default {
+  name: 'App',
+
+  components: {
+    GoogleMapComponent,
+  },
+};
+</script>
---
+++ src/components/GoogleMapComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <GoogleMap
+    :api-key="getGoogleMapKeys"
+    :mapId="getMapId"
+    class="my-map"
+    :center="center"
+    :zoom="15"
+  >
+    <AdvancedMarker :options="markerOptions" :pin-options="pinOptions" />
+  </GoogleMap>
+</template>
+
+<script>
+import { GoogleMap, AdvancedMarker } from 'vue3-google-map';
+
+export default {
+  name: 'GoogleMapComponent',
+
+  components: {
+    GoogleMap,
+    AdvancedMarker,
+  },
+
+  data() {
+    return {
+      center: { lat: 40.689247, lng: -74.044502 },
+      markerOptions: { position: this.center, title: 'LADY LIBERTY' },
+      pinOptions: { background: '#FBBC04' },
+    };
+  },
+
+  computed: {
+    getGoogleMapKeys() {
+      return import.meta.env.VITE_GOOGLE_API_KEY;
+    },
+
+    getMapId() {
+      return import.meta.env.VITE_DEMO_MAP_ID;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.my-map {
+  width: 100%;
+  height: 600px;
+}
+</style>
